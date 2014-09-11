2cfd5a32af1ac1d6b2e05868b25e6fa7bf2ac1c2
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-09-10 21:31:31 -0400

    adding tpls for user login

---
+++ sites/all/themes/freighthouse/template/page--user--login.tpl.php
@@ -0,0 +1,20 @@
+<?php global $base_url;?>
+<!-- Intro loader -->
+<div class="mask">
+    <div id="intro-loader"></div>
+</div>
+<!-- Intro loader -->
+
+<!-- Login Section -->
+<section id="login" class="intro-pattern nf-pattern">
+    <div class="text-home">
+        <div class="intro-item">
+            <div class="section-body">
+            </div>
+            <div class="mybutton ultra">
+                <a class="start-button" href="<?php print $base_url;?>"> <span data-hover="Back to home"><?php print t('Back to home');?></span> </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Login Section -->
