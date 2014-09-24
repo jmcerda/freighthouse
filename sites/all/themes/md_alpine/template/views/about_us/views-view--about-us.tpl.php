7fbd07c8cc241dd3de899f2fec3d19d81e35672d
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-09-24 18:17:50 -0400

    formatting about block

--- sites/all/themes/md_alpine/template/views/about_us/views-view--about-us.tpl.php
+++ sites/all/themes/md_alpine/template/views/about_us/views-view--about-us.tpl.php
@@ -54,14 +54,14 @@
         </div>
     <?php endif; ?>
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 about_image">
             <div class="element-line">
                 <?php if(theme_get_setting('team_image_path')):?>
                 <img src="<?php print md_alpine_theme_setting_check_path(theme_get_setting('team_image_path'));?>" class="img-responsive img-center" alt="">
                 <?php endif;?>
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 about_text">
             <div class="element-line">
                 <span>
                     <?php
