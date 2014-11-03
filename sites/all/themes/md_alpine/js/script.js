42694aa7920bf4acd78e3da526cb8569b0e1952f
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-11-03 15:45:24 -0500

    fix socialtarget

--- sites/all/themes/md_alpine/js/script.js
+++ sites/all/themes/md_alpine/js/script.js
@@ -377,7 +377,7 @@
         //moving number animation to overview
         $('#numberTarget>div').append( $('#numberMove') );
         //moving connect to footer
-        $('#socialTarget>div').append( $('#socialMove') );
+        $('#socialTarget').append( $('#socialMove') );
 
         //Navigation Dropdown
         $('.nav a.int-collapse-menu').click(function() {
