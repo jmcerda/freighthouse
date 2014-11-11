45664f626d13ea278f4f7e902c6d4deacf45d2e1
john <jcristobal@freighthousemedia.com>
2014-11-11 21:38:54 +0800

    Adding animation to buttons css -staging

--- sites/all/themes/freighthouse/template.php
+++ sites/all/themes/freighthouse/template.php
@@ -20,8 +20,8 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
                 $form['actions']['submit']['#value'] = t('Login');
                 $form['actions']['submit']['#prefix'] = '<div class="row">
               <div class="col-md-12 text-center">
-                  <div class="action mybutton medium"><span style="position:relative; display:inline-block;" data-hover="Login">';
-                $form['actions']['submit']['#suffix'] = '</span></div>
+                  <div class="action mybutton medium"><button onclick="this.submit();"><span style="position:relative; display:inline-block;" data-hover="Login">';
+                $form['actions']['submit']['#suffix'] = '</span></button></div>
               </div>
             </div>';
                 break;
@@ -36,8 +36,8 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
                 $form['actions']['submit']['#value'] = t('Sign up');
                 $form['actions']['submit']['#prefix'] = '<div class="row">
               <div class="col-md-12 text-center">
-                  <div class="action mybutton medium"><span style="position:relative; display:inline-block;" data-hover="Sign up">';
-                $form['actions']['submit']['#suffix'] = '</span></div>
+                  <div class="action mybutton medium"><button onclick="this.submit();"><span style="position:relative; display:inline-block;" data-hover="Sign up">';
+                $form['actions']['submit']['#suffix'] = '</span></button></div>
               </div>
             </div>';
                 break;
