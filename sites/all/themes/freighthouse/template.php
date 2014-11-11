f276e645180a669bfe688ea4ee2561c2df5430fb
john <jcristobal@freighthousemedia.com>
2014-11-11 14:20:58 +0800

    Adding animation to buttons css -test

--- sites/all/themes/freighthouse/template.php
+++ sites/all/themes/freighthouse/template.php
@@ -86,8 +86,8 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
         $form['actions']['submit']['#value'] = 'Send Message';
         $form['actions']['submit']['#prefix'] = '<div class="row">
               <div class="col-md-12 text-center">
-                <div class="action mybutton medium"><span data-hover="SEND MESSAGE">';
-        $form['actions']['submit']['#sufix'] = '</span></div>
+                <div class="action mybutton medium"><a><span data-hover="SEND MESSAGE">';
+        $form['actions']['submit']['#sufix'] = '</span></a></div>
                 </div>
               </div>
             ';    
