6ac94dca619b9810296a92cdc3ea08888ca7e4e4
“John <jcristobal@freighthousemedia.com>
2014-11-14 12:52:26 +0800

    custom js code fix

--- sites/all/themes/freighthouse/js/script-freighthouse.js
+++ sites/all/themes/freighthouse/js/script-freighthouse.js
@@ -1,15 +1,16 @@
  //john uncommented to make li element active on specific part of frontpage
- 
+ var basePath = Drupal.settings.basePath;
+  
  if($("body").hasClass("front")) {
- $("ul.navbar-nav li").each(function(){
- var link = $(this).find("a");
- link.removeClass("active");
- var href = link.attr('href');
- if(href.search("#") != -1) {
- var newHref = href.replace(basePath,'');
- link.attr('href',newHref);
- }
- })
+	$("ul.navbar-nav li").each(function(){
+		var link = $(this).find("a");
+		link.removeClass("active");
+		var href = link.attr('href');
+		if(href.search("#") != -1) {
+		var newHref = href.replace(basePath,'');
+		link.attr('href',newHref);
+		}
+	})
  }
  
  //end uncomment
