168da8106dc2f92a18dd20d66010f0675d6704a3
“John <jcristobal@freighthousemedia.com>
2014-11-16 00:51:38 +0800

    timezone -staging

--- sites/all/themes/freighthouse/template/views/contact/views-view--contact.tpl.php
+++ sites/all/themes/freighthouse/template/views/contact/views-view--contact.tpl.php
@@ -39,27 +39,27 @@
  <div class="section-title text-center">
   <div class="item_fade_in" style="opacity: 1; right: 0px;">
     <div class="col-md-2 col-sm-2 col-xs-12 timeclock">
-    	<canvas id="timezone_NYC" class="CoolClock:swissRail:80::-4"></canvas>
+    	<canvas id="timezone_NYC" class="CoolClock:freightHouse:80::-4"></canvas>
         <span>-NYC-</span>
     </div>
     <div class="col-md-2 col-sm-2 col-xs-12 timeclock">
-    	<canvas id="timezone_MEX" class="CoolClock:swissRail:80::-6"></canvas>
+    	<canvas id="timezone_MEX" class="CoolClock:freightHouse:80::-6"></canvas>
         <span>-MEX-</span>
     </div>
     <div class="col-md-2 col-sm-2 col-xs-12 timeclock">
-    	<canvas id="timezone_RU" class="CoolClock:swissRail:80::+7"></canvas>
+    	<canvas id="timezone_RU" class="CoolClock:freightHouse:80::+7"></canvas>
         <span>-RU-</span>
     </div>
     <div class="col-md-2 col-sm-2 col-xs-12 timeclock">
-    	<canvas id="timezone_AMS" class="CoolClock:swissRail:80::+1"></canvas>
+    	<canvas id="timezone_AMS" class="CoolClock:freightHouse:80::+1"></canvas>
         <span>-AMS-</span>
     </div>
     <div class="col-md-2 col-sm-2 col-xs-12 timeclock">
-    	<canvas id="timezone_MNL" class="CoolClock:swissRail:80::+8"></canvas>
+    	<canvas id="timezone_MNL" class="CoolClock:freightHouse:80::+8"></canvas>
         <span>-MNL-</span>
     </div>
     <div class="col-md-2 col-sm-2 col-xs-12 timeclock">
-    	<canvas id="timezone_IND" class="CoolClock:swissRail:80::+5"></canvas>
+    	<canvas id="timezone_IND" class="CoolClock:freightHouse:80::+5"></canvas>
         <span>-IND-</span>
     </div>
    </div>
