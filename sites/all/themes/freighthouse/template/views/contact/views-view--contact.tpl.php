13c0b635c16680468b9ea2ebbad3d65d2359e768
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-11-20 15:07:31 -0500

    ...

--- sites/all/themes/freighthouse/template/views/contact/views-view--contact.tpl.php
+++ sites/all/themes/freighthouse/template/views/contact/views-view--contact.tpl.php
@@ -38,17 +38,17 @@
 <!-- clock time zone -->
 <div class="parallax-content">
  <div class="row">
-    <div class="col-md-6 col-sm-4 col-xs-2 timeclock">
+    <div class="col-md-2 col-sm-2 col-xs-2 timeclock">
         <div class="item_fade_in" style="opacity: 1; right: 0px;">
-            <canvas id="timezone_SFO" class="CoolClock:simple:40::-8:00"></canvas>
+            <canvas id="timezone_SFO" class="CoolClock:freightHouse:40::-8:00"></canvas>
             <div><h4>SFO</h4></div>
         </div>
         <div class="item_fade_in" style="opacity: 1; right: 0px;">
-            <canvas id="timezone_MCI" class="CoolClock:simple:40::-6"></canvas>
+            <canvas id="timezone_MCI" class="CoolClock:freightHouse:40::-6"></canvas>
             <div><h4>MCI</h4></div>
         </div>
         <div class="item_fade_in" style="opacity: 1; right: 0px;">
-            <canvas id="timezone_MEX" class="CoolClock:simple:40::-6"></canvas>
+            <canvas id="timezone_MEX" class="CoolClock:freightHouse:40::-6"></canvas>
             <div><h4>MEX</h4></div>
         </div>
         <div class="item_fade_in" style="opacity: 1; right: 0px;">
@@ -56,15 +56,15 @@
             <div><h4>WAS</h4></div>
         </div>
         <div class="item_fade_in" style="opacity: 1; right: 0px;">
-            <canvas id="timezone_NYC" class="CoolClock:simple:40::-5"></canvas>
+            <canvas id="timezone_NYC" class="CoolClock:freightHouse:40::-5"></canvas>
             <div><h4>NYC</h4></div>
         </div>
         <div class="item_fade_in" style="opacity: 1; right: 0px;">
-            <canvas id="timezone_AMS" class="CoolClock:simple:40::+1"></canvas>
+            <canvas id="timezone_AMS" class="CoolClock:freightHouse:40::+1"></canvas>
             <div><h4>AMS</h4></div>
         </div>
         <div class="item_fade_in" style="opacity: 1; right: 0px;">
-            <canvas id="timezone_MNL" class="CoolClock:simple:40::+8"></canvas>
+            <canvas id="timezone_MNL" class="CoolClock:freightHouse:40::+8"></canvas>
             <div><h4>MNL</h4></div>
         </div>
     </div>
