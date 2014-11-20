38aec8a4f00f1997f3b395f949157f92336e378b
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-11-20 15:36:23 -0500

    oops

--- sites/all/themes/freighthouse/template/page--front.tpl.php
+++ sites/all/themes/freighthouse/template/page--front.tpl.php
@@ -144,7 +144,7 @@
 <?php endif;?>
     <section id="assoc" class="section-content">
         <img class="lazy" src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/Drupal_Association_org_mem.png" alt="drupal association"/>
-        <img class="lazy src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/freelancersunion_member.png" alt="freelancer union"/>
+        <img class="lazy" src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/freelancersunion_member.png" alt="freelancer union"/>
     </section>
 
 <?php if($page['footer']):?>
