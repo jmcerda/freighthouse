0c85bd32d10d124d7c2eb5d6f8404e88a2dca836
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-11-21 16:57:23 -0500

    ..

--- sites/all/themes/freighthouse/template/page--front.tpl.php
+++ sites/all/themes/freighthouse/template/page--front.tpl.php
@@ -142,10 +142,13 @@
     </section>
     <!-- Contact Section -->
 <?php endif;?>
-    <div class="col-md-6 col-sm-12 col-xs-12">
     <section id="assoc" class="section-content">
-        <img class="lazy" src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/Drupal_Association_org_mem.png" alt="drupal association"/>
-        <img class="lazy" src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/freelancersunion_member.png" alt="freelancer union"/>
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <img class="lazy" src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/Drupal_Association_org_mem.png" alt="drupal association"/>
+        </div>
+        <div class="col-md-6 col-sm-12 col-xs-12">
+            <img class="lazy" src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/freelancersunion_member.png" alt="freelancer union"/>
+        </div>
     </section>
     </div>
 
