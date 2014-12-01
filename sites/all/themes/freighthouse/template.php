6e14823c576df289179d5437fe427e7f6f2f8570
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-12-01 16:26:51 -0500

    ...

--- sites/all/themes/freighthouse/template.php
+++ sites/all/themes/freighthouse/template.php
@@ -40,13 +40,13 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
     if (strpos($form_id,"webform_client_form") === false) {
         switch ($form_id) {
             case 'user_login':
-                // $form['name']['#attributes']['class'][] = 'form-control input-lg';
-                // $form['name']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
-                // $form['name']['#suffix'] = '</div>';
-                // $form['pass']['#attributes']['class'][] = 'form-control input-lg';
-                // $form['pass']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
-                // $form['pass']['#suffix'] = '</div>';
-                // $form['actions']['submit']['#value'] = t('Login');
+                $form['name']['#attributes']['class'][] = 'form-control input-lg';
+                $form['name']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
+                $form['name']['#suffix'] = '</div>';
+                $form['pass']['#attributes']['class'][] = 'form-control input-lg';
+                $form['pass']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
+                $form['pass']['#suffix'] = '</div>';
+                $form['actions']['submit']['#value'] = t('Login');
                 $form['actions']['submit']['#prefix'] = '<div class="row">
               <div class="col-md-12 text-center">
                   <div class="action mybutton medium"><button onclick="this.submit();"><span style="position:relative; display:inline-block; " data-hover="Login">';
@@ -55,14 +55,13 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
             </div>';
                 break;
             case 'user_register_form':
-                // $form['account']['name']['#attributes']['class'][] = 'form-control input-lg';
-                // $form['account']['name']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
-                // $form['account']['name']['#suffix'] = '</div>';
-                // $form['account']['mail']['#attributes']['class'][] = 'form-control input-lg';
-                // $form['account']['mail']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
-                // $form['account']['mail']['#suffix'] = '</div>';
-
-                // $form['actions']['submit']['#value'] = t('Sign up');
+                $form['account']['name']['#attributes']['class'][] = 'form-control input-lg';
+                $form['account']['name']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
+                $form['account']['name']['#suffix'] = '</div>';
+                $form['account']['mail']['#attributes']['class'][] = 'form-control input-lg';
+                $form['account']['mail']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
+                $form['account']['mail']['#suffix'] = '</div>';
+                $form['actions']['submit']['#value'] = t('Sign up');
                 $form['actions']['submit']['#prefix'] = '<div class="row">
               <div class="col-md-12 text-center">
                   <div class="action mybutton medium"><button onclick="this.submit();"><span style="position:relative; display:inline-block; " data-hover="Sign up">';
@@ -71,13 +70,13 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
             </div>';
                 break;
             case 'user_login_block':
-                // $form['name']['#attributes']['class'][] = 'form-control input-lg';
-                // $form['name']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
-                // $form['name']['#suffix'] = '</div>';
-                // $form['pass']['#attributes']['class'][] = 'form-control input-lg';
-                // $form['pass']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
-                // $form['pass']['#suffix'] = '</div>';
-                // $form['actions']['submit']['#value'] = t('Login');
+                $form['name']['#attributes']['class'][] = 'form-control input-lg';
+                $form['name']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
+                $form['name']['#suffix'] = '</div>';
+                $form['pass']['#attributes']['class'][] = 'form-control input-lg';
+                $form['pass']['#prefix'] = '<div class="col-md-12 col-sm-12 col-md-12 col-xs-12">';
+                $form['pass']['#suffix'] = '</div>';
+                $form['actions']['submit']['#value'] = t('Login');
                 $form['actions']['submit']['#prefix'] = '<div class="row">
               <div class="col-md-12 text-center">
                   <div class="action mybutton medium"><span>';
@@ -99,7 +98,6 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
                 break;
         }
     } else {
-        /*
         $form['#attributes']['class'][] = 'element-inline';
         $form['actions']['submit']['#value'] = 'Send Message';
         $form['actions']['submit']['#prefix'] = '<div class="row">
@@ -110,7 +108,6 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
                 </div>
               </div>
             ';
-        */
         $form['#attributes']['class'][] = 'element-line';
         $form['actions']['submit']['#value'] = 'Send Message';
         $form['actions']['submit']['#prefix'] = '<div class="row">
