51f3a69b7b6358c760df03e4a4e1a14ca274c322
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-12-09 13:30:29 -0500

    forcing ext sublime js to footer

--- sites/all/themes/freighthouse/template.php
+++ sites/all/themes/freighthouse/template.php
@@ -122,7 +122,11 @@ function freighthouse_form_alter(&$form, &$form_state, $form_id) {
     }
 }
 
-drupal_add_js('https://cdn.sublimevideo.net/js/gk9qliwk.js', 'external');
+
+
+drupal_add_js('https://cdn.sublimevideo.net/js/gk9qliwk.js', 'external',
+  array('type' => 'inline', 'scope' => 'footer', 'weight' => 5)
+  );
 
 /**
  * Theme the username/password description of the user login form
