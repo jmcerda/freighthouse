1a4558617bd60cfcfe288f2b905d4138e184bb2f
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-12-27 19:19:05 -0500

    ...

--- sites/all/themes/freighthouse/template/user-register-form.tpl.php
+++ sites/all/themes/freighthouse/template/user-register-form.tpl.php
@@ -3,16 +3,16 @@
    so we can put in links above */
 
     // print drupal_render($form['pass']);
-    print drupal_render($form['form_build_id']);
-    print drupal_render($form['form_id']);
-    print drupal_render($form['actions']);
+    // print drupal_render($form['form_build_id']);
+    // print drupal_render($form['form_id']);
+    // print drupal_render($form['actions']);
 
 ?>
 
-<div class="login-photo">
-    <?php print drupal_render($form['name']); ?>
-    <?php print drupal_render($form['pass']); ?>
-   <!--  <img src="<?php print base_path() . drupal_get_path('theme', 'freighthouse')
-   . '/images/login-photo.jpg'; ?>"
+<!-- <div class="login-photo"> -->
+    <?php // print drupal_render($form['name']); ?>
+    <?php // print drupal_render($form['pass']); ?>
+   <!--  <img src="<?php //print base_path() . drupal_get_path('theme', 'freighthouse')
+   //. '/images/login-photo.jpg'; ?>"
         alt="Login" title="Login" width='327' height='221' /> -->
-</div>
+<!-- </div> -->
