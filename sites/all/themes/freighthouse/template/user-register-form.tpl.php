fefdeb9b7f55bf56a77508edae891fc77905d528
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-12-27 19:16:40 -0500

    ...

--- sites/all/themes/freighthouse/template/user-register-form.tpl.php
+++ sites/all/themes/freighthouse/template/user-register-form.tpl.php
@@ -2,8 +2,7 @@
  /* split the username and password from the submit button
    so we can put in links above */
 
-    print drupal_render($form['name']);
-    print drupal_render($form['pass']);
+    // print drupal_render($form['pass']);
     print drupal_render($form['form_build_id']);
     print drupal_render($form['form_id']);
     print drupal_render($form['actions']);
@@ -11,6 +10,8 @@
 ?>
 
 <div class="login-photo">
+    <?php print drupal_render($form['name']); ?>
+    <?php print drupal_render($form['pass']); ?>
    <!--  <img src="<?php print base_path() . drupal_get_path('theme', 'freighthouse')
    . '/images/login-photo.jpg'; ?>"
         alt="Login" title="Login" width='327' height='221' /> -->
