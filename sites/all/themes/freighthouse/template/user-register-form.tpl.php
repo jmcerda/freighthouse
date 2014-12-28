b83bcec076d90a0af3cb156a20dba6c259c5f8c1
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2014-12-27 19:13:09 -0500

    modifying user login

--- sites/all/themes/freighthouse/template/user-register-form.tpl.php
+++ sites/all/themes/freighthouse/template/user-register-form.tpl.php
@@ -1,5 +1,17 @@
 <?php
-  print '<pre>';
-  print_r($variables);
-  print '</pre>';
+ /* split the username and password from the submit button
+   so we can put in links above */
+
+    print drupal_render($form['name']);
+    print drupal_render($form['pass']);
+    print drupal_render($form['form_build_id']);
+    print drupal_render($form['form_id']);
+    print drupal_render($form['actions']);
+
 ?>
+
+<div class="login-photo">
+   <!--  <img src="<?php print base_path() . drupal_get_path('theme', 'freighthouse')
+   . '/images/login-photo.jpg'; ?>"
+        alt="Login" title="Login" width='327' height='221' /> -->
+</div>
