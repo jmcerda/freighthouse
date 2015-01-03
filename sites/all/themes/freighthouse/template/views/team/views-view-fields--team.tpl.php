198edcba4f10fc80519969860c7b5391ed9b8ff9
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2015-01-03 15:07:41 -0500

    team tpl fixes

--- sites/all/themes/freighthouse/template/views/team/views-view-fields--team.tpl.php
+++ sites/all/themes/freighthouse/template/views/team/views-view-fields--team.tpl.php
@@ -29,7 +29,7 @@ $social = $row->field_field_team_socials;
 
 ?>
 <!-- Team item -->
-<div class="col-md-2 col-sm-2 col-md-2 col-xs-12">
+<div class="col-md-1 col-sm-1 col-md-1 col-xs-12">
     <div class="element-line">
         <div class="item_top">
             <div class="img-rounded team-element zoom">
