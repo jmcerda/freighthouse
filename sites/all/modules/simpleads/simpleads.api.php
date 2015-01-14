b76dc4f1637de41b76c1147714dc737c4e8c9afd
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2015-01-14 18:13:50 -0500

    adding simpleads dev 2.x

---
+++ sites/all/modules/simpleads/simpleads.api.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * SimpleAds API.
+ */
+
+/**
+ * Implements hook_insert_click().
+ */
+function hook_insert_click($node, $ip_address) {
+
+}
+
+/**
+ * Implements hook_insert_impression().
+ */
+function hook_insert_impression($node, $ip_address) {
+
+}
+
+/**
+ * Implements hook_simpleads_activate().
+ */
+function hook_simpleads_activate($node) {
+
+}
+
+/**
+ * Implements hook_simpleads_expired().
+ */
+function hook_simpleads_expired($node) {
+
+}
+
+/**
+ * Implements hook_simpleads_types_alter().
+ */
+function hook_simpleads_types_alter(&$types) {
+
+}
