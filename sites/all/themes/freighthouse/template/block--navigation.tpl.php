22d4186b5f1b576c07af41c0f35f0e2b55743211
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2015-01-18 01:33:53 -0500

    logo fix

--- sites/all/themes/freighthouse/template/block--navigation.tpl.php
+++ sites/all/themes/freighthouse/template/block--navigation.tpl.php
@@ -5,7 +5,7 @@ if(theme_get_setting('logo_normal_path')) {
 } else {
     $logo_path = $base_url.'/'.drupal_get_path('theme','md_alpine').'/img/logo.png';
 }
- $logo_path = $base_url.'/'.drupal_get_path('theme','freighthouse').'/images/fh_logo40.png';
+ $logo_path = $base_url.'/'.drupal_get_path('theme','freighthouse').'/images/fh_logo100.png';
 ?>
 <div id="navigation" class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="navbar-inner">
@@ -14,7 +14,7 @@ if(theme_get_setting('logo_normal_path')) {
                 <span class="sr-only">Toggle navigation</span>
                 <i class="icon fontello icon-menu icon-2x"></i>
             </button>
-            <a id="brand" class="navbar-brand" href="<?php print $base_url;?>"> <img src="<?php print $logo_path;?>" alt=""/> </a>
+            <a id="brand" class="navbar-brand" href="<?php print $base_url;?>"> <img src="<?php print $logo_path;?>" alt="Freighthouse"/> </a>
             <div class="mybutton small cta item_fade_in">
                 <a class="start-button colorbox-node cta_button" href="get-help"><span data-hover="Do Work">New Project</span></a>
             </div>
