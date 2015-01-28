c1a634c1a54ddd973797ca6d3c5973132fa0896e
Jeremy Michael Cerda <jeremy@freighthousemedia.com>
2015-01-27 20:35:43 -0500

    working on support form link

--- sites/all/themes/freighthouse/template/page--front.tpl.php
+++ sites/all/themes/freighthouse/template/page--front.tpl.php
@@ -142,7 +142,6 @@
     </section>
     <!-- Contact Section -->
 <?php endif;?>
-    <div =id"supportlink"></div>
     <div class="footer">
         <div class="col-lg-2 mobile-hide">
             <img id="assoc-left" class="lazy" src="<?php echo drupal_get_path('theme', 'freighthouse')?>/images/Drupal_Association_org_mem.png" alt="drupal association"/>
